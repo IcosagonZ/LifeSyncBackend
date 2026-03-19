0796f5934bd3342ef515d333e386fe7b0e550b17
IcosagonZ <rohanraj3870z@gmail.com>
2026-03-19 09:16:21 +0530

    Modified read_data_vitals

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+# FastAPI backend for LifeSyncAI
+# Run using fastapi dev
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -9,9 +14,32 @@ def read_root():
         "version": "0.1.0"
     }
 
-@app.get("/client/version/{client_version}")
-def read_client_version(client_version: str | None = None):
-    print("Client > Version {}".format(client_version))
+@app.post("/client/version")
+async def read_client_version(request: Request):
+    data = await request.json();
+    print("Client>Data {}".format(data))
+    return {
+        "status": "OK",
+        "version": "0.1.0"
+    }
+
+# Vitals data processing
+class VitalsData(BaseModel):
+  type: str
+  value: str
+  unit: str
+  entry_date: str
+  entry_note: str
+
+class VitalsDataRequest(BaseModel):
+    version: str
+    data: List[VitalsData]
+
+@app.post("/data/vitals")
+async def read_data_vitals(payload: VitalsDataRequest):
+    print("Client>Version: {}".format(payload.version))
+    print("Client>Data: Received {} rows".format(len(payload.data)))
+
     return {
         "status": "OK",
         "version": "0.1.0"
